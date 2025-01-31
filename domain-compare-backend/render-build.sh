5fb3246ebda6bf2ad0f168bf9975aae763c770b4
newsyd04 <newsyd04@gmail.com>
2025-01-31 11:04:35 +0000

    fixing render deploy

--- domain-compare-backend/render-build.sh
+++ domain-compare-backend/render-build.sh
@@ -5,21 +5,22 @@ set -o errexit
 # Install dependencies
 npm install
 
-# Uncomment the following line if you have a separate build process
-# npm run build
-
 # Ensure the Puppeteer cache directory exists
-PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer
+PUPPETEER_CACHE_DIR=/opt/render/.cache/puppeteer/chrome
+PROJECT_CACHE_DIR=/opt/render/project/src/.cache/puppeteer/chrome
+
+# Create directories if they don't exist
 mkdir -p $PUPPETEER_CACHE_DIR
+mkdir -p $PROJECT_CACHE_DIR
 
 # Install Puppeteer and download Chrome
 npx puppeteer browsers install chrome
 
-# Store or pull Puppeteer cache with build cache
+# Copy Puppeteer cache to/from project directory
 if [[ -d $PUPPETEER_CACHE_DIR ]]; then
     echo "...Copying Puppeteer Cache from Build Cache"
-    cp -R $PUPPETEER_CACHE_DIR /opt/render/project/src/.cache/puppeteer/chrome/
+    cp -R $PUPPETEER_CACHE_DIR/* $PROJECT_CACHE_DIR
 else
     echo "...Storing Puppeteer Cache in Build Cache"
-    cp -R /opt/render/project/src/.cache/puppeteer/chrome/ $PUPPETEER_CACHE_DIR
+    cp -R $PROJECT_CACHE_DIR/* $PUPPETEER_CACHE_DIR
 fi
